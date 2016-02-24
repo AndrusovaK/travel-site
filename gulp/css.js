8dd69df110888435ff57566b85ecfcc761f275f0
AndrusovaK <andrusova_ksenia@mail.ru>
2016-02-24 17:29:19 +0300

    Update 2016-02-24T14:29:03.073Z

--- gulp/css.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp'),
-    config = require('./config');
-
-gulp.task('css', function() {
-  console.log('---------- копирование CSS');
-  return gulp.src('src/less/vendor/*.css')
-    .pipe(gulp.dest('dist/css/vendor'));
-});
--- gulp/webserver.js
+++
@@ -1,8 +0,0 @@
-/* BrowserSync local web server*/
-var gulp = require('gulp'),
-    config = require('./config'),
-    browserSync = require("browser-sync");
-
-gulp.task('webserver', ['build'], function () {
-    browserSync(config.browserSync);
-});
