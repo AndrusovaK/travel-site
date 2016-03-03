aebc97b68b58805075da89ca875a25246e86fb89
AndrusovaK <andrusova_ksenia@mail.ru>
2016-03-03 22:48:28 +0300

    Update 2016-03-03T19:48:23.906Z

--- js/custom/custom-bundle.js
+++ js/custom/custom-bundle.js
@@ -268,10 +268,11 @@ sr.reveal('.sr-delay-045', {delay: 450});
 
 //Табы и пагинация страницы галереи
 
-(function (){
+$(function (){
 
 	var url = 'gallery-europe.html';
 	var Paging;
+
 		//Собственно пагинация
 
 	function pagination() {
@@ -411,45 +412,6 @@ sr.reveal('.sr-delay-045', {delay: 450});
 		pagination();
 	});
 
-})();
+});
 
 
-/*(function(){
-
-	var navItem = $(".gallery-nav__link");
-
-	navItem.on("click", function(e){
-		e.preventDefault();
-		//переставляем класс активной ссылки
-		navItem.removeClass('gallery-nav__link--active');
-		$(this).addClass('gallery-nav__link--active');
-
-		//генерируем по клику url для запроса AJAX
-		var tabUrl = $(this).data("tab");
-		var url = 'gallery-' + tabUrl + '.html';
-		// console.log(url);
-
-		//Собственно пагинация
-
-		var prev = {
-			start: 0,
-			stop: 0
-		};
-
-		var content = $.ajax({
-			url: url,
-			async: false,
-			dataType: 'html',
-			error: function(){
-				console.log('loading error');
-			},
-			success: function(data){
-				console.log('load was performed');
-				console.log(url);
-			}
-		}).responseText.split('<br/>');
-
-		console.log(content);
-
-	});
-})();*/
